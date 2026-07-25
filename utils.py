d2e9baadea66622d7438aeec74deac0b7a010ef7
yalliguttiravi3-commits <yalliguttiravi3@gmail.com>
2026-07-25 13:37:35 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 join_db = JoinReqs
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))")
 
-imdb = Cinemagoer() 
+# imdb = Cinemagoer() 
 TOKENS = {}
 VERIFIED = {}
 BANNED = {}
