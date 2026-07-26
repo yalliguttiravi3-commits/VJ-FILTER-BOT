071e6083c5d1b6cd85cbd87b6c1ed9d4affabbd8
yalliguttiravi3-commits <yalliguttiravi3@gmail.com>
2026-07-26 15:54:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,9 +67,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
-F_DB_URI = environ.get('F_DB_URI', "")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "")   # This Db is for File Data Store When First Db Is Going To Be Full.
+O_DB_URI = environ.get('O_DB_URI', "DATABASE_URI")   # This Db Is For Other Data Store
+F_DB_URI = environ.get('F_DB_URI', "DATABASE_URI")   # This Db Is For File Data Store
+S_DB_URI = environ.get('S_DB_URI', "DATABASE_URI")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
