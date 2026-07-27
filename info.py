8605d3f1b5e481fc7b711c0420352900d1fbed81
yalliguttiravi3-commits <yalliguttiravi3@gmail.com>
2026-07-27 11:42:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -128,8 +128,8 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://kalinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'c5f50c2b94f8489775d29a6c03f777c4459cc4e5')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://arolinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '44f3e542af81aab6e1e1c7dd7a920feffccd65d4')
 TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
